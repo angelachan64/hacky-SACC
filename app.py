30493761695a14314ba4b077788aafd11746436f
angelachan64 <angelachan64@gmail.com>
2015-11-24 13:08:48 -0500

    not working

--- app.py
+++ app.py
@@ -8,26 +8,25 @@ app = Flask(__name__)
 def home():
 	if request.method == "POST":
 		q = request.form['query']
-		results = google.search(q, num=10, start=0, stop=10)
-		rlist = []
-		for r in results:
+        results = google.search(q, num=10, start=0, stop=10)
+        rlist = []
+        for r in results:
 		    rlist.append(r)
-
-		url = urllib2.urlopen(rlist[0])
-		page = url.read()
-		soup = bs4.BeautifulSoup(page,'html5lib')
-		raw = soup.get_text()
-		#text = re.sub("[ \t\n]+"," ",raw)
-		text = raw
-		#print text
-		return render_template("home.html", results=results)
-	else:
-		return render_template("home.html")
-
+        url = urllib2.urlopen(rlist[0])
+        page = url.read()
+        soup = bs4.BeautifulSoup(page,'html5lib')
+        raw = soup.get_text()
+        #text = re.sub("[ \t\n]+"," ",raw)
+        text = raw
+        #print text
+        return render_template("home.html", query=q, results=results)
+    else:
+        return render_template("home.html")
+    
 @app.route("/results")
 def results():
-	return render_template("results.html")
-
+    return render_template("results.html")
+    
 @app.route("/about")
 def about():
     return render_template("about.html")
